f4c9d4c1c36ea627bbd08652fb62ae1e5072b5ea
pvitto98 <62291477+pvitto98@users.noreply.github.com>
2020-11-22 12:34:42 +0100

    Update caltech_dataset.py

--- caltech_dataset.py
+++ caltech_dataset.py
@@ -9,7 +9,7 @@ import pandas as pd
 
 def pil_loader(path):
     # open path as file to avoid ResourceWarning (https://github.com/python-pillow/Pillow/issues/835)
-    with open(path, 'rb') as f:
+    with open("/content/Caltech101/101_ObjectCategories/"+path, 'rb') as f:
         img = Image.open(f)
         return img.convert('RGB')
 
@@ -29,43 +29,49 @@ class Caltech(VisionDataset):
           through the index
         - Labels should start from 0, so for Caltech you will have lables 0...100 (excluding the background class) 
         '''
-        
-        
-        self.txt_list = self.split+".txt"    #cosí ho la lista dei file che adopereró ["train.txt", "test.txt"]
-        df = pd.read_csv("/content/Caltech101/"+self.txt_list, sep=' ', index_col=0) #leggo i file
-        #con index_col=0 impongo pandas a mettere il label all'indice 0
-        #sep ' ' é il separatore 
+        self.txt_list = self.split+".txt"    #train.txt or test.txt
+        df = pd.read_csv("/content/Caltech101/"+self.txt_list, sep=' ', index_col=0)
         self.img_names = df.index.values
-        self.root = root #in root é presente la locazione del direttorio principale
-        self.transform = transform #viene salvata la trasformazione da effettuare al dataset, al momento nessuna
-        classes, class_to_idx = self._find_classes(self.root) #trovo le classi
-        self.classes = classes #salvo le classi
-        self.class_to_idx = class_to_idx #meccanismo per passare da classe ad indice
+        self.root = root
+        self.transform = transform
+        classes, class_to_idx = self._find_classes(self.root)
+        self.classes = classes
+        self.class_to_idx = class_to_idx
         self.samples = []
         for i in self.img_names:
-            c_name = i.split("/")[0] #prendo la prima parte del nome
-            if(c_name != "BACKGROUND_Google"): #se non equivale alla classe BACKGROUND_google (che é una classe da scartare)
-                c_id = class_to_idx[c_name] #trovo il suo id
-                self.samples.append(tuple((i, c_id))) # e lo metto nella lista di samples
-    
+            c_name = i.split("/")[0]
+            if(c_name != "BACKGROUND_Google"):
+                c_id = class_to_idx[c_name]
+                self.samples.append(tuple((i, c_id)))
+
+    def get_image_from_folder(self, name):
+        """
+        gets a image by a name gathered from file list text file
+
+        :param name: name of targeted image
+        :return: a PIL image
+        """
+
+        image = Image.open(os.path.join(self.root, name))
+        return image
+
     def _find_classes(self, dir: str):
-            """
-            Finds the class folders in a dataset.
+        """
+        Finds the class folders in a dataset.
 
-            Args:
-                dir (string): Root directory path.
+        Args:
+            dir (string): Root directory path.
 
-            Returns:
-                tuple: (classes, class_to_idx) where classes are relative to (dir), and class_to_idx is a dictionary.
+        Returns:
+            tuple: (classes, class_to_idx) where classes are relative to (dir), and class_to_idx is a dictionary.
 
-            Ensures:
-                No class is a subdirectory of another.
-            """
-            classes = [d.name for d in os.scandir(dir) if d.is_dir() and d.name != "BACKGROUND_Google"]
-            classes.sort()
-            class_to_idx = {cls_name: i for i, cls_name in enumerate(classes)}
-            return classes, class_to_idx
-        
+        Ensures:
+            No class is a subdirectory of another.
+        """
+        classes = [d.name for d in os.scandir(dir) if d.is_dir() and d.name != "BACKGROUND_Google"]
+        classes.sort()
+        class_to_idx = {cls_name: i for i, cls_name in enumerate(classes)}
+        return classes, class_to_idx
 
     def __getitem__(self, index):
         '''
@@ -78,11 +84,10 @@ class Caltech(VisionDataset):
         '''
 
         image, label = self.samples[index] # Provide a way to access image and label via index
-                           # Image should be a PIL Image
-                           # label can be int
+                                           # Image should be a PIL Image
+                                           # label can be int
+        image = pil_loader(image)
 
-        image = pil_loader(image) 
-        
         # Applies preprocessing when accessing the image
         if self.transform is not None:
             image = self.transform(image)
@@ -94,5 +99,5 @@ class Caltech(VisionDataset):
         The __len__ method returns the length of the dataset
         It is mandatory, as this is used by several other components
         '''
-          # Provide a way to get the length (number of elements) of the dataset
-        return len(self.samples)
+        length = len(self.samples) # Provide a way to get the length (number of elements) of the dataset
+        return length
